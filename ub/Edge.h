e922d4cd1622b773b5a8fb671e3fa027fa0ac685
Solveig Andres <s9soandr@users.noreply.github.com>
2014-11-07 16:05:37 +0100

    Methoden in Edge.h hinzugefügt
    Konstruktoren, Methoden, Membervariablen  hinzugefügt

--- ub/Edge.h
+++ ub/Edge.h
@@ -10,8 +10,44 @@
 
 class Edge {
 public:
+    /*
+     * Default - Konstruktor
+     */
 	Edge();
+    
+    /*
+     * Copy - Konstruktor
+     */
+    Edge(const Edge& edge);
+    
+    /*
+     * Destruktor
+     */
 	virtual ~Edge();
+    
+    /*
+     * Zuweisungsoperator
+     */
+    
+    
+    /* 
+     * liefert den Ausgangsknoten der Kante zurueck
+     */
+    const Node& getSource() const
+    
+    /*
+     * liefert den Zielknoten der Kante zurueck
+     */
+    const Node& getTarget() const
+    
+    /*
+     * liefert die Ueberlappung der Knoten zurueck
+     */
+    unsigned int getOverlap() const
+    
+private:
+    Node& source;
+    Node& target;
 };
 
 #endif /* EDGE_H_ */
