c8d6e74ca6ea3d63582f1a809907876dd02d940c
Solveig Andres <s9soandr@users.noreply.github.com>
2014-11-07 20:10:12 +0100

    Konstruktoren geändert (noch falsch) [Edge.cpp]

--- ub/Edge.cpp
+++ ub/Edge.cpp
@@ -5,27 +5,45 @@
 /*
  * Default - Konstruktor
  */
-Edge::Edge(){
+Edge::Edge():Edge{
     Edge::target=0;
     Edge::source=0;
 }
-
-
 /*
  * Copy - Konstruktor
  */
-Edge::Edge(const Edge& edge){
-    Edge::target=edge:getTarget();
-    Edge::source=edge:getSource();
+Edge::Edge(const Edge& edge):edge(Edge){
+    Edge::target=edge.getTarget();
+    Edge::source=edge.getSource();
 }
 
 /*
  * Detailed - Konstruktor
  */
-Edge(const Node& s, const Node& t){
+Edge(Node& s,Node& t){
     Edge::source=s;
     Edge::target=t;
 }
+
+/*
+ * Zuweisungsoperatorator "="
+ */
+Edge& operator=(const Edge& edge){
+    Edge::target=edge.getTarget();
+    Edge::source=edge.getSource()
+}
+
+/*
+ * Operator "=="
+ */
+bool operator==(const Edge& edge) const;
+
+/*
+ * Operator "!="
+ */
+bool operator!=(const Edge& edge) const;
+
+
 
 
 
