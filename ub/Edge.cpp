1fe67fe4d757b19821f209f572afd7165ec891e1
Solveig Andres <s9soandr@users.noreply.github.com>
2014-11-07 19:53:40 +0100

    Destruktoren hinzugefügt (noch falsch) [Edge.cpp]

--- ub/Edge.cpp
+++ ub/Edge.cpp
@@ -5,9 +5,33 @@
 /*
  * Default - Konstruktor
  */
-Edge(){
-    
+Edge::Edge(){
+    Edge::target=0;
+    Edge::source=0;
 }
+
+
+/*
+ * Copy - Konstruktor
+ */
+Edge::Edge(const Edge& edge){
+    Edge::target=edge:getTarget();
+    Edge::source=edge:getSource();
+}
+
+/*
+ * Detailed - Konstruktor
+ */
+Edge(const Node& s, const Node& t){
+    Edge::source=s;
+    Edge::target=t;
+}
+
+
+
+
+
+
 
 Edge::~Edge() {
 	// TODO Auto-generated destructor stub
