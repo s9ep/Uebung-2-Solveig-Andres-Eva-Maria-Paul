3cd4f1c5204c82ca5e2335c0edb8836dfad22963
Solveig Andres <s9soandr@users.noreply.github.com>
2014-11-08 14:51:14 +0100

    Sequence.cpp neue Version hochgeladen

--- ub/Sequence.cpp
+++ ub/Sequence.cpp
@@ -3,48 +3,120 @@
 #include <iostream>
 #include <string>
 #include <stdio.h>
+#include <cstring>
 
-template <class InputIterator1, class InputIterator2>
 
-//default constructor
+
+// Default constructor
 Sequence::Sequence(){
-	}
+}
 
-//destructor
+
+//Copy Constructor
+Sequence::Sequence(const Sequence& seq){
+    sequence_ = seq.sequence_;
+}
+
+//Detailed Constructor
+Sequence::Sequence(const std::string& seq){
+    sequence_ = seq;
+}
+
+//Destructor
 Sequence::~ Sequence (){
-	
-	}
-//copy constructor
-Sequence(const Sequence& seq){
-	sequence_ = seq.sequence_;
-	}
-
-//detailed constructor
-Sequence(const std::string& seq){
-	sequence_ = seq;
+    
 }
 
-void Sequence::clear(){
-	sequence_.clear();
+
+//operator "="
+Sequence& Sequence::operator=(const Sequence& seq)
+{
+    sequence_ = seq.sequence_;
+    return *this;
 }
 
-bool Sequence::compare(const Sequence& seq, unsigned int i) {
-	int begin = sequence_.length()-i;
-	if begin <1 {
-		return false;
-	}
-	else {
-		std::string first1;
-		first1 = sequence_.c_str()+i;
-		std::string first2;
-		first2 = seq.sequence_.c_str();
-		if (strcmp (first1, first2)==0) {
-			return true;		
-		}else{
-			return false;
-		}
-	}	
+//operator "=="
+bool Sequence::operator==(const Sequence& seq) const
+{
+    return sequence_ == seq.sequence_;
 }
+
+//operator "!="
+bool Sequence::operator!=(const Sequence& seq) const
+{
+    return !(*this == seq);
+}
+
+//operator "<<"
+std::ostream& operator<<(std::ostream& ostr, const Sequence& seq)
+{
+    ostr << seq.sequence_ << ' ';
+    return ostr;
+}
+
+//operator ">>"
+std::istream& operator>>(std::istream& istr, Sequence& seq)
+{
+    if (istr.good()){
+        char first = istr.get(); //first char
+        if (!isspace(first)){ //space
+            //       istr.putback(c);
+            istr >> seq.sequence_;
+            first = istr.peek(); //Returns the next character in the input sequence, without extracting it: The character is left as the next character to be extracted from the stream.
+            if (istr.good() && isspace(first)){
+                first = istr.get();
+            }
+            return istr;
+        }
+    }
+    return istr;
+}
+
+
+
+// COMPARE
+bool Sequence::compare(const Sequence& seq, unsigned int i) const {
+    
+    // startpoint
+    int begin = sequence_.length()-i;
+    
+    // empty sequence
+    if (begin <1) {
+        return false;
+    }
+    else {
+        if ( memcmp(sequence_.c_str() + i, seq.sequence_.c_str(), begin) == 0)
+        { return true;}
+        else
+        {return false;}
+    }
+}
+
+//OVERLAP
+unsigned int Sequence::overlap(const Sequence& seq) const{
+    unsigned int max=0;
+    // Empty seq
+    if (seq.sequence_.length()==0) {
+        return max;
+    }
+    
+    // Empty sequence_
+    if (sequence_.length()==0) {
+        return max;
+    }
+    // Same length, just compare
+    if (compare(seq,0)) {
+        return seq.sequence_.length();
+    }
+    
+    unsigned int position_sequence_=sequence_.length()-1;
+    unsigned int position_seq =0;
+    
+    
+    return 0;
+}
+
+
 
 
 
