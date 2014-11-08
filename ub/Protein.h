5c090927ac8c67f3515dfa4ae47e84693aca1e38
Solveig Andres <s9soandr@users.noreply.github.com>
2014-11-08 14:48:39 +0100

    Protein.h neue Version hochgeladen

--- ub/Protein.h
+++ ub/Protein.h
@@ -4,22 +4,20 @@
 #include "Sequence.h"
 
 class Protein : public Sequence{
+    
 public:
-
-	//defautl constructor
-	Protein();
-
-	//copy constr
-	explicit Protein (const Protein& seq);
-
-	//detailed constr
-	Protein(const std::string& seq);
-
-	//is Valid
-
-	virtual bool isValid() const;
-
-
+    //Default Constructor
+    Protein();
+    
+    //Copy Constructor
+    explicit Protein (const Protein& seq);
+    
+    //Detailed Constructor
+    Protein(const std::string& seq);
+    
+    //is Valid
+    virtual bool isValid() const;
+    
 };
 
 #endif
