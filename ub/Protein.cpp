af33ec7212eeb4e97fc008e5ebef257e68cdcc3a
Solveig Andres <s9soandr@users.noreply.github.com>
2014-11-08 14:47:39 +0100

    Protein.cpp neue Version hochgeladen

--- ub/Protein.cpp
+++ ub/Protein.cpp
@@ -2,19 +2,20 @@
 
 //default constructor
 Protein::Protein(){
-	}
-
-//copy constructor
-Protein(const Protein& seq):Sequence{
-	}
-
-bool Protein::isValid()const{
-return sequence_.find_first_not_of("ACDEFGHIKLMNPQRSTVWY")==std::string::npos;
 }
 
-/**
-//destructor
-Protein::~Protein(){
-	}
-	
-**/
+//copy constructor
+Protein::Protein(const Protein& seq):Sequence(seq){
+}
+
+//detailed constructor
+Protein::Protein(const std::string& seq)
+: Sequence(seq)
+{
+}
+
+//isValid
+bool Protein::isValid()const{
+    return sequence_.find_first_not_of("ACDEFGHIKLMNPQRSTVWY")==std::string::npos;
+}
+
