d74178357958a47cbed017d486e92903cbbd519c
Solveig Andres <s9soandr@users.noreply.github.com>
2014-11-08 12:23:05 +0100

    Fehlende Methoden hinzugefügt [Node.h]

--- ub/Node.h
+++ ub/Node.h
@@ -16,9 +16,19 @@ public:
 	Node();
     
     /*
+     * Copy - Konstruktor
+     */
+    Node(const Node& node);
+    
+    /*
+     * Detailed Konstruktor
+     */
+    explicit Node(const Sequence& sequence);
+    
+    /*
      * Destruktor
      */
-	virtual ~Node();
+    virtual ~Node();
     
     /*
      * Zuweisungsoperatorator "="
@@ -34,6 +44,23 @@ public:
      * Operator "!="
      */
     bool operator!=(const Node& node) const;
+    
+    /*
+     * Gibt die Liste aller ausgehenden Kanten zurück
+     */
+    std::vector<Edge> getOutEdges() const;
+    
+    /*
+     * erstellt eine Kante von diesem zum übergebenen Knoten mit korrekten Kantengewicht
+     */
+    Edge buildEdgeTo(Node &node);
+    
+    /*
+     * falls eine ausgehende Kante zu node besteht, wird diese entfernt
+     */
+    void removeEdgeTo(Node& node);
+    
+    
 
     
     
