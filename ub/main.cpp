1cd6beb28700f623be4693f894e7d4c334ff17ce
Solveig Andres <s9soandr@users.noreply.github.com>
2014-11-10 08:51:27 +0100

    Graph.dot Datei öffnen in Main hinzugefügt

--- ub/main.cpp
+++ ub/main.cpp
@@ -1,9 +1,20 @@
 #include "Graph.h"
 #include "Node.h"
 #include "Edge.h"
+#include <stdio.h>
 
 int main(int argc, char* argv[]){
     
+    // Zeiger auf Datei
+    FILE *f;
+    
+    // Datei oeffnen
+    f = fopen("graph.dot", "rwb");
+    if (f)
+    {
+        // TODO Grah ausgeben
+        fclose(f);
+    }
     
     return 0;
 }
